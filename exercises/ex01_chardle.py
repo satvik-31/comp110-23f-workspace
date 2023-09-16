988dca0a6c00e6bb137b13d68e7ed7c18a08e0e0
Satvik Chethan <satvik31@ad.unc.edu>
2023-09-15 20:48:28 -0400

    Corrected spaces

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,4 +1,4 @@
-"""EX01 - Creating Chardle"""
+"""EX01 - Creating Chardle."""
 
 __author__ = "730517765"
 
@@ -20,23 +20,23 @@ print("Searching for " + user_name_2 + " in " + user_name)
 number_of_instances: int = 0
 
 if user_name_2 == user_name[0]:
-    print(user_name_2 + str (" found at index 0 "))
+    print(user_name_2 + str(" found at index 0 "))
     number_of_instances = number_of_instances + 1
 
 if user_name_2 == user_name[1]:
-    print(user_name_2 + str (" found at index 1 "))
+    print(user_name_2 + str(" found at index 1 "))
     number_of_instances = number_of_instances + 1 
 
 if user_name_2 == user_name[2]:
-    print(user_name_2 + str (" found at index 2 "))
+    print(user_name_2 + str(" found at index 2 "))
     number_of_instances = number_of_instances + 1
 
 if user_name_2 == user_name[3]:
-    print(user_name_2 + str (" found at index 3 "))
+    print(user_name_2 + str(" found at index 3 "))
     number_of_instances = number_of_instances + 1
 
 if user_name_2 == user_name[4]:
-    print(user_name_2 + str (" found at index 4 "))
+    print(user_name_2 + str(" found at index 4 "))
     number_of_instances = number_of_instances + 1
 
 if number_of_instances == 1:
@@ -44,7 +44,5 @@ if number_of_instances == 1:
 if number_of_instances > 1:
     print(str(number_of_instances) + " instances of " + user_name_2 + " found in " + user_name)
 if number_of_instances == 0:
-    print( "No instances of " + user_name_2 + " found in " + user_name)
-
-
+    print("No instances of " + user_name_2 + " found in " + user_name)
 
