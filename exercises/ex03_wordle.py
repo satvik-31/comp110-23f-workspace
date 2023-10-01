7aad6c103aeb072747b26ffaa84b5f347aaf7fb1
Satvik Chethan <satvik31@ad.unc.edu>
2023-10-01 18:28:51 -0400

    After gradescope

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,50 +1,55 @@
-"""Creating Wordle"""
+"""Creating Wordle!"""
 
-__author__: "730517765"
+__author__: str = "730517765"
+
 
 def main() -> None:
-    """The entrypoint of the program and main game loop"""
+    """The entrypoint of the program and main game loop."""
     turn_no: int = 1
     attempt: bool = False
     secret: str = "codes"
     while turn_no <= 6 and attempt is not True:
-        print (f" === Turn {turn_no}/6 === ")
+        print(f" === Turn {turn_no}/6 === ")
         guess: str = input_guess (len(secret))
-        print (emojified(guess, secret))
+        print(emojified(guess, secret))
         if guess == secret:
-            print (f"You won in {turn_no}/6 turns! ")
+            print(f"You won in {turn_no}/6 turns! ")
             attempt = True
         turn_no = turn_no + 1
     if attempt is False:
         print("X/6 - Sorry, try again tomorrow")
 
+
 def contains_char(any_length: str, single_char: str ) -> bool:
-    """To find out if the letter belongs in the word"""
+    """To find out if the letter belongs in the word."""
     assert len(single_char) == 1
+    validity: bool = False
     char_idx: int = 0
-    while char_idx < len(any_length):
+    while validity is False and char_idx < len(any_length):
         if single_char == any_length[char_idx]:
-            return True
-        char_idx = char_idx + 1
-    else: 
-        return False
-        
+            validity is True
+        else: 
+            validity is False
+            char_idx = char_idx + 1
+
+
 white_box: str = "\U00002B1C"
 green_box: str = "\U0001F7E9"
 yellow_box: str = "\U0001F7E8"
 
+
 def emojified(guess: str, secret:str) -> str:
-    """To find out the placement of the correct letters"""
-    assert len(guess) == len(secret)
+    """To find out the placement of the correct letters."""
+    assert len (guess) == len (secret)
     emoji: str = ""
     guess_idx: int = 0
 
-    while guess_idx < len(secret):
+    while guess_idx < len (secret):
         if guess[guess_idx] == secret[guess_idx]:
             emoji = emoji + green_box
         
-        else: 
-            if contains_char (secret , guess[guess_idx]):
+        else:
+            if contains_char(secret , guess[guess_idx]) :
                 emoji = emoji + yellow_box
 
             else:
@@ -52,12 +57,14 @@ def emojified(guess: str, secret:str) -> str:
         guess_idx += 1
     return emoji
 
+
 def input_guess(expected_length: int) -> str:
-    """Prompt the user for a guess until they provide guess of expected length"""
+    """Prompt the user for a guess until they provide guess of expected length."""
     first_try: str = input(f"Enter a {expected_length} character word: ")
     while len(first_try) != expected_length:
         first_try = input (f"That wasn't {expected_length} characters! Try Again: ")
     return first_try
+
 
 if __name__ == "__main__":
     main()            
