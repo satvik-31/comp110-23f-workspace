b7e83f18f74fbe602f4ecce85caef2b2ef563db2
Satvik Chethan <satvik31@ad.unc.edu>
2023-10-01 20:02:49 -0400

    Latest

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -10,7 +10,7 @@ def main() -> None:
     secret: str = "codes"
     while turn_no <= 6 and attempt is not True:
         print(f"=== Turn {turn_no}/6 ===")
-        guess: str = input_guess (len(secret))
+        guess: str = input_guess(len(secret))
         print(emojified(guess, secret))
         if guess == secret:
             print(f"You won in {turn_no}/6 turns! ")
@@ -20,12 +20,12 @@ def main() -> None:
         print("X/6 - Sorry, try again tomorrow")
 
 
-def contains_char(any_length: str, single_char: str ) -> bool:
+def contains_char(any_length: str, single_char: str )-> bool:
     """To find out if the letter belongs in the word."""
     assert len(single_char) == 1
     validity: bool = False
     char_idx: int = 0
-    while validity is False and char_idx < len(any_length) :
+    while validity is False and char_idx < len(any_length):
         if single_char == any_length[char_idx]:
             validity = True
         else: 
@@ -38,18 +38,18 @@ green_box: str = "\U0001F7E9"
 yellow_box: str = "\U0001F7E8"
 
 
-def emojified(guess: str, secret:str) -> str:
+def emojified(guess: str, secret:str) -> str: 
     """To find out the placement of the correct letters."""
-    assert len (guess) == len (secret)
+    assert len(guess) == len(secret)
     emoji: str = ""
     guess_idx: int = 0
 
-    while guess_idx < len (secret):
+    while guess_idx < len(secret):
         if guess[guess_idx] == secret[guess_idx]:
             emoji = emoji + green_box
         
         else:
-            if contains_char(secret , guess[guess_idx]) :
+            if contains_char(secret, guess[guess_idx]):
                 emoji = emoji + yellow_box
 
             else:
@@ -62,7 +62,7 @@ def input_guess(expected_length: int) -> str:
     """Prompt the user for a guess until they provide guess of expected length."""
     first_try: str = input(f"Enter a {expected_length} character word: ")
     while len(first_try) != expected_length:
-        first_try = input (f"That wasn't {expected_length} characters! Try Again: ")
+        first_try = input(f"That wasn't {expected_length} characters! Try Again: ")
     return first_try
 
 
