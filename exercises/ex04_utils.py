98ddbc89da3d496e0c7b98f08c25fe97edcfb899
Satvik Chethan <satvik31@ad.unc.edu>
2023-10-10 22:07:49 -0400

    change

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,24 +1,24 @@
-"""Utils"""
+"""Utils!"""
 
-___author__: str ="730517765"
+___author__: str = "730517765"
 
 def all(options: list[int], the_one: int) -> bool:
-    """Finds if every number in the list is equal to one outside"""
+    """Finds if every number in the list is equal to one outside!"""
     idx: int = 0
     while idx < len(options):
         if the_one == options[idx]:
             idx = idx + 1  
         else: 
-           return False
+            return False
     if len(options) == 0:
         return False
     return True
         
 def max(large: list[int])->int:
-    """Given a list of functions, returns the largest value"""
+    """Given a list of functions, returns the largest value!"""
     if len(large) == 0:
         raise ValueError("max() arg is an empty List")
-#Use the same logic as the "current_card" and "lowest_card" example from class
+# Use the same logic as the "current_card" and "lowest_card" example from class
     indx: int = 0
     big = large[0]
     while indx < len(large):
@@ -27,10 +27,15 @@ def max(large: list[int])->int:
         indx += 1
     return big
 
-def is_equal(list1: list[int], list2: list[int])->bool:
-    """Checks if the two lists are equal to each other"""
+def is_equal(list1: list[int], list2: list[int])-> bool:
+    """Checks if the two lists are equal to each other!"""
     ildx: int = 0
-    while ildx < len(list1) and ildx < len(list2):
+    if len(list1) > len(list2):
+        small = list2
+    else:
+        small = list1
+
+    while ildx < len(small):
         if list1[ildx] == list2[ildx]:
             ildx += 1
         else: 
